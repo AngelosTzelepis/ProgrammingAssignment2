58cf4ffe37ec54eb42f80a47c6615b0035e6fd8e
Angelos Tzelepis <agtzelepis@gmail.com>
2019-01-05 22:31:46 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## "makeCacheMatrix" defines other functions to 
+##      use for storing and retrieving matrix data.
+## "cacheSolve" uses those functions to know
+##      whether to return cached data or to perform
+##      the required calculation itself
 
-## Write a short comment describing this function
+
+
+## The input x is assumed to be an invertible matrix
+## Return a list of functions to the working environment
+##      to set the matrix, get the matrix, set the inverse 
+##      of the matrix, and get the inverse of the matrix
+## This list of functions becomes the input to cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # initialize the empty cache
+    cache <- NULL
+    
+    # set the matrix 
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    
+    # get the matrix
+    get <- function() x
+    
+    # invert and cache the matrix
+    setinv <- function(solve) cache <<- solve 
+    
+    # get the cached inverted data
+    getinv <- function() cache
+    
+    # return the functions
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## x is the returned list of functions from makeCacheMatrix()
+## This function returns the inverse of the original matrix
+##      from cache if already calculated. If not, it calculates 
+##      the inverse using "solve" and stores it in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # Load inverted matrix data using getinv function
+    # as defined above. May be empty.
+    cache = x$getinv()
+    
+    # If inverse has already been calculated (is not null)
+    # return that cached data and exit function
+    if (!is.null(cache)){
+        message("getting cached data")
+        return(cache)
+    }
+    
+    # If not, invert the matrix 
+    matrix = x$get()
+    cache = solve(matrix, ...)
+    
+    # Caches the cache matrix with setinv function
+    x$setinv(cache)
+    
+    return(cache)
 }
